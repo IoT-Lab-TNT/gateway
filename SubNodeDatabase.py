c973e42b08cb075175b33e44966cc693c21a33e1
IoT-Lab-TNT <iot.02@tnt.associates>
2020-10-01 16:01:25 +0700

    SubNodeDatabase.py

--- SubNodeDatabase.py
+++ SubNodeDatabase.py
@@ -1,37 +1,61 @@
 import paho.mqtt.client as mqtt
 import json
-import random
-from time import sleep
-from Insert import Sensors
+import mysql.connector
 
-# MQTT setting
-MQTT_Broker = "192.168.1.244"
-MQTT_Port = 1883
-Keep_Alive_Interval = 7200
-MQTT_Topic = 'thean'
+THINGSBOARD_HOST = 'tntholdings.ddns.net'
+ACCESS_TOKEN = 'GatewayF20_Token'
 
-sw_data = {}
-# Callback server
 
-def on_connect(client, userdata, flags, rc):
-    if rc != 0:
-        pass
-    else:
-        print("Connected! ")
-    print("Connection returned result: " + str(MQTT_Broker))
-    mqttc.subscribe(MQTT_Topic, qos = 1)
-    print("da subscribe")
 
-# Callback on_message server
+def on_connect(client, userdata, rc, *extra_params):
+    client.subscribe('v1/devices/me/rpc/request/+')
+
+
 def on_message(client, userdata, msg):
-    Sensors(msg.payload) 
-    print("Message Recieved from tang 20: "+msg.payload.decode())
+    print ('Topic: ' + msg.topic + '\nMessage: ' + str(msg.payload))
+    data = json.loads(msg.payload)
+    data2 = json.loads(data['params'])
 
+    if data['method'] == 'InsertDevice':
+        con =  ConnectToMysql ('localhost', 'trung', 'raspberry', 'TNTHOLDINGS')
+        print ("Connected!")
+        for x in data2:
+            print (CheckExistsDevice (con, x['deviceId']))
+           # InsertDeviceToMysql (con, x['deviceId'], x['deviceName'], x['deviceLocation'], x['chanelId'])
+        print ("Inserted!")
+
+def ConnectToMysql (host, userName, password, dataBase):
+    myconn = mysql.connector.connect(host = host, user = userName, password = password, database = dataBase)
+    print ("Database connected!")
+    return myconn
+
+def InsertDeviceToMysql (sqlConnector, deviceId, deviceName, deviceLocation, chanelId):
+    cursor = sqlConnector.cursor()
+    insertDeviceSQL = "INSERT INTO device (deviceId, deviceName, deviceLocation, chanelId) VALUES (%s, %s, %s, %s)"
+    cursor.execute(insertDeviceSQL, (deviceId, deviceName, deviceLocation, chanelId))
+    sqlConnector.commit()
+    #sqlConnector.close()
+
+def CheckExistsDevice (sqlConnector, deviceId):
+    check = False
+    cursor = sqlConnector.cursor(buffered=True)
+    print (deviceId, type(deviceId))
+    SQL = "SELECT COUNT(1) FROM device WHERE deviceId = %s"
+    id = (deviceId,)
+    cursor.execute(SQL, id)
+    sqlConnector.commit()
+    if cursor.fetchone()[0]:
+        check = True
+    return check
     
+#############################################################################################################################################################################
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.username_pw_set(ACCESS_TOKEN)
+client.connect(THINGSBOARD_HOST, 1883, 60)
 
-mqttc = mqtt.Client()
-mqttc.on_connect = on_connect
-mqttc.on_message = on_message
-#mqttc.username_pw_set(username = "thean", password = "qdLpQewK")
-mqttc.connect(MQTT_Broker, MQTT_Port, Keep_Alive_Interval)
-mqttc.loop_forever()
+try:
+    client.loop_forever()
+except KeyboardInterrupt:
+    client.disconnect()
